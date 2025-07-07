babf6b7fb3649cec643571ec7c9f96f04c0c13d2
Muhyiddin Syarif <82252062+YI-SYA@users.noreply.github.com>
2025-07-07 12:42:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,7 @@ def audio():
     return send_file("response.mp3", mimetype="audio/mpeg")
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
